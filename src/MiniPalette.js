2d74a14b608b2823ad6aab344a57c636b6adf918
yung0512 <jeff8651215@gmail.com>
2021-04-04 21:02:51 +0800

    finish styling minipalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -13,7 +13,11 @@ const styles = {
         }
     },
     colors:{
-       
+        backgroundColor:"grey",
+        height:"150px",
+        width:"100%",
+        borderRadius:"5px",
+        overflow:"hidden"
     },
     title:{
         display:"flex",
@@ -28,15 +32,31 @@ const styles = {
     emoji:{
        marginLeft:"0.5rem",
        fontSize:"1.5rem" 
+    },
+    miniColor:{
+        height:"25%",
+        width:"20%",
+        display:"inline-block",
+        margin:"0 auto",
+        position:"relative",
+        marginBottom:"-3.5px"
     }
 }
 function MiniPalette(props){
-    const {classes,paletteName,emoji} = props;//the material ui will generate a classes object each class name has identical key value like class id
+    const {classes,paletteName,emoji,colors} = props;//the material ui will generate a classes object each class name has identical key value like class id
+    const miniColorBoxes = colors.map(color=>(
+        <div 
+            className={classes.miniColor} 
+            style={{backgroundColor:color.color}}
+            key={color.name}
+        />
+    ))
     return(
         <div className={classes.root}>
-            <div className={classes.colors}>
-                <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span ></h5>
-            </div>
+            <div className={classes.colors}>{miniColorBoxes}</div>
+            <h5 className={classes.title}>{paletteName}
+                <span className={classes.emoji}>{emoji}</span >
+            </h5>           
         </div>
     );
 }

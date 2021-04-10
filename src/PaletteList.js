6695c216bf2c897ed9b85af094a1589f65fdc6db
yung0512 <jeff8651215@gmail.com>
2021-04-10 13:34:45 +0800

    manage github page

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,8 +2,22 @@ import {Link} from 'react-router-dom'
 import React ,{Component} from 'react'
 import { CSSTransition,TransitionGroup,} from 'react-transition-group';
 import MiniPalette from './MiniPalette'
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import blue from '@material-ui/colors/blue'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import DialogTitle from "@material-ui/core/DialogTitle";
 import {withStyles} from "@material-ui/styles"
 import styles from './styles/PaletteListStyles'
+
+
 class PaletteList extends Component {
     goToPalette(id){
         this.props.history.push(`/palette/${id}`)
@@ -35,6 +49,27 @@ class PaletteList extends Component {
                                     ))}
                         </TransitionGroup>
                 </div>
+                <Dialog open={true}>
+                    <DialogTitle>Delete This Palette?</DialogTitle>
+                    <List>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <CheckIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>Delete</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <CheckIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>Cancel</ListItemText>
+                        </ListItem>
+                    </List>
+                </Dialog>
             </div>
         )
     }
